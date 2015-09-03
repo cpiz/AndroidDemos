818090e802b2b87b5ac623ea23c6784e3ede9f2c
DoubleCai <caipiz@gmail.com>
2015-09-03 22:54:24 +0800

    改进Retrofit测试用例，先从缓存查询，再从服务端查询

--- app/src/main/java/com/cpiz/android/playground/RetrofitTest/RetrofitTestActivity.java
+++ app/src/main/java/com/cpiz/android/playground/RetrofitTest/RetrofitTestActivity.java
@@ -6,57 +6,79 @@ import com.cpiz.android.playground.BaseTestActivity;
 import com.cpiz.android.utils.RxServicesFactory;
 import com.google.gson.Gson;
 
+import java.util.concurrent.TimeUnit;
+
 import retrofit.http.GET;
 import retrofit.http.Path;
 import rx.Observable;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Retrofit使用
- *
+ * <p/>
  * Created by caijw on 2015/9/3.
  */
 public class RetrofitTestActivity extends BaseTestActivity {
     private static final String TAG = "RetrofitTestActivity";
+    private GitUser mGitUser = null;
 
     @Override
     public void onLeftClick() {
-        RxServicesFactory.getService(IService.class).getUser("cpiz")
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe(new Action0() {
+        Observable<GitUser> obCache = Observable.create(new Observable.OnSubscribe<GitUser>() {
+            @Override
+            public void call(Subscriber<? super GitUser> subscriber) {
+                subscriber.onNext(mGitUser);
+                subscriber.onCompleted();
+            }
+        });
+
+        Observable<GitUser> obRemote = RxServicesFactory.getService(IService.class).getUser("cpiz").doOnNext(new Action1<GitUser>() {
+            @Override
+            public void call(GitUser gitUser) {
+                mGitUser = gitUser;
+                Log.i(TAG, "set data to cache");
+            }
+        });
+
+        Observable.concat(obCache, obRemote)
+                .first(new Func1<GitUser, Boolean>() {
                     @Override
-                    public void call() {
-                        appendLine("doOnSubscribe");
+                    public Boolean call(GitUser gitUser) {
+                        return gitUser != null;
                     }
                 })
+                .timeout(5000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
                 .doOnUnsubscribe(new Action0() {
                     @Override
                     public void call() {
-                        appendLine("doOnUnsubscribe");
-                    }
-                })
-                .doOnTerminate(new Action0() {
-                    @Override
-                    public void call() {
-                        appendLine("doOnTerminate");
+                        Log.i(TAG, "doOnUnsubscribe");
                     }
                 })
                 .subscribe(new Action1<GitUser>() {
                     @Override
                     public void call(GitUser gitUser) {
-                        appendLine("GitUsf: " + new Gson().toJson(gitUser));
+                        Log.i(TAG, String.format("data = %s", gitUser));
+                        appendLine("data: " + new Gson().toJson(gitUser));
                     }
                 }, new Action1<Throwable>() {
                     @Override
                     public void call(Throwable throwable) {
-                        appendLine(String.format("Get git user info failed, error=%s", throwable));
+                        Log.i(TAG, "Error occured", throwable);
+                        appendLine(String.format("Get data failed, error=%s", throwable));
                     }
                 });
     }
 
     static class GitUser {
+        public GitUser() {
+            Log.i(TAG, "GitUser constructed");
+        }
+
         String login;
         int id;
         String avatar_url;
